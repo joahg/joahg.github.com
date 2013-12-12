a5577fd41b7a2b1a62af4fc91eb653d42bdd6847
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-12 09:44:25 -0600

    Added additional Google Analytic call to compensate for dynamic targeting

--- js/script.js
+++ js/script.js
@@ -60,6 +60,16 @@ $(document).ready(function(){
 		$("body").scrollTop(0);
 		$("nav a.active").removeClass("active");
 		$(this).addClass("active");
+
+		// Google Analytics
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-45765973-1', 'joahg.com');
+		ga('send', 'pageview');
+		// END Google Analytics
 	});
 
 	if (document.location.hash === "" || document.getElementById(document.location.hash.substr(1,document.location.hash.length-1)) == null) {
